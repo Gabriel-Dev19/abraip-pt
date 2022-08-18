1d31db20957a353f12b92a1af8f47fe4a452297d
DESKTOP-NOHUJUV\Rosiane Queiroz <dyeybson@gmail.com>
2022-08-18 13:16:51 -0300

    Atualizações do número de whatsapp

--- src/components/buttons/btn-outline-ligue-agora.vue
+++ src/components/buttons/btn-outline-ligue-agora.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <a href="tel:++351913342032" class="btn btn-outline-dark px-25 d-flex align-items-center justify-content-center fw-600 border-radius-20">
+        <a href="tel:+351913342032" class="btn btn-outline-dark px-25 d-flex align-items-center justify-content-center fw-600 border-radius-20">
             <i class="fa fa-phone-alt mr-2 fw-400"></i> Ligue agora
         </a>
     </div>
